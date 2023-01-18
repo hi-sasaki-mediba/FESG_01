3a68532f4f85eae1f998ae71b31a21b85f8836cb
hi-sasaki-mediba <hi-sasaki@mediba.jp>
2023-01-18 17:41:27 +0900

    prettier write

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,7 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
