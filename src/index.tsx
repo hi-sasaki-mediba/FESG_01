28a14f9e10542ceffc01b76521409e169b8fc78b
a-yoshizawa-mediba <a-yoshizawa@mediba.jp>
2023-01-26 16:11:30 +0900

    add theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -8,13 +8,14 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import { Provider, useSelector } from 'react-redux';
+import store, { RootState } from './store/store';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { NotFound } from './features/error/NotFound';
 import { ThemeProvider } from '@mui/system';
 import { createTheme } from '@mui/material';
+import { ChangeTheme } from './component/changeTheme';
 
 const router = createBrowserRouter([
   {
@@ -22,21 +23,30 @@ const router = createBrowserRouter([
     element: <App />,
     errorElement: <NotFound />,
   },
+  {
+    path: '/theme',
+    element: <ChangeTheme />,
+  },
 ]);
 
-const theme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+const WrappedThemeProvider = ({ children }: { children: JSX.Element }) => {
+  const { theme: mode } = useSelector((state: RootState) => state.theme);
+  const theme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
+      <WrappedThemeProvider>
         <RouterProvider router={router} />
-      </ThemeProvider>
+      </WrappedThemeProvider>
     </Provider>
   </React.StrictMode>
 );
